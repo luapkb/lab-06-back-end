4f60191c99af6b540c5a03e2f1b818455c8e0ad4
paul bennett <luapkb@gmail.com>
2019-10-22 11:47:08 -0700

    update

--- server.js
+++ server.js
@@ -2,15 +2,42 @@
 
 const express = require('express');
 
+const cors = require('cors');
+
 require('dotenv').config();
 
-const app =express();
+const app = express();
+app.use(cors());
 
 const PORT = process.env.PORT || 3003;
-//routes 
-app.length('*', (request, response) => {
+//routes
+app.get('/location', (request, response) => {
+
   const city = request.query.data;
-  response.send (city);
-})
+
+  const locationData = searchLatToLong(city);
+
+
+  response.send(locationData);
+
+});
+
+
+app.get('*', (request, resoponse) => {
+  resoponse.status(404).send('somepen wrong here');
+});
+
+function searchLatToLong(lacation){
+  const geoData = require('./data/jeo.jason');
+  console.log(geoData);
+
+  const locationObj = {
+    search_query: location,
+    formatted_query: geoData.results[0].formatted_address,
+    latitude: geoData.results[0].location.lat,
+    longitude: geoData.results[0].location.lng,
+  }
+
+}
 
 app.listen(PORT, () => console.log(`I have been heard on ${PORT}`));
