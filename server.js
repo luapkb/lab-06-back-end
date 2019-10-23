27e6d43432c5c62e52dbec2f5857aea4e1d9e285
paul bennett <luapkb@gmail.com>
2019-10-22 20:40:34 -0700

    sandeye almost there

--- server.js
+++ server.js
@@ -11,30 +11,40 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3003;
 //routes
-app.get('/location', (request, response) => {
-
+app.get('/searchLatToLong', (request, response) => {
   const city = request.query.data;
+  const latlong = searchLatToLong(city);
 
-  const locationData = searchLatToLong(city);
-
-
-  response.send(locationData);
-
+  response.send(latlong,'LOOK AT ME..');
 });
+app.get('/')
 
-
-app.get('*', (request, resoponse) => {
+app.get('/weather', (request, resoponse) => {
+  const darkskyData = require('./data/darksky.json')
+  const tempArray = []
   resoponse.status(404).send('somepen wrong here');
+
+  darkskydata.daily.data.forEach (object => {
+    let tempValue = new Weather (object);
+    tempArray.push(tempValue); 
+  })
 });
 
-function searchLatToLong(lacation){
+function Wearther (object) {
+  this.forcast = object.summary;
+  this.time = this.revisedDate(object.time)
+}
+Wearther.prototype.revisedDate = funtion (time) {
+  let dat
+}
+function searchLatToLong(location){
   const geoData = require('./data/jeo.jason');
   console.log(geoData);
-const locationObj = new Location(location,geoData);
+const locationObj = new latlong(location,geoData);
 return locationObj;
 }
-function Location(city, geoData);{
-  this.search_query =city;
+function latlong(city, geoData);{
+  this.search_query = City;
   this.formatted_query = geoData.results[0].formatted_address;
   this.latitude = geoData.results[0].geometry.location.lat;
   this.longitude =  geoData.results[0].geometry.location.lng;
